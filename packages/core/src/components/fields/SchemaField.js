3cefcda9fd7be7293f52d9eb8c508a38ba6a15b3
Jakob Klepp <jakob.klepp@moonvision.io>
2022-06-23 17:49:36 +0200

    Fix Bug: Custom field that is anyOf displays twice
    https://github.com/rjsf-team/react-jsonschema-form/issues/2691

--- packages/core/src/components/fields/SchemaField.js
+++ packages/core/src/components/fields/SchemaField.js
@@ -14,6 +14,8 @@ import {
   getDisplayLabel,
 } from "../../utils";
 
+const NullComponent = () => null;
+
 const REQUIRED_FIELD_SYMBOL = "*";
 const COMPONENT_TYPES = {
   array: "ArrayField",
@@ -39,7 +41,7 @@ function getFieldComponent(schema, uiSchema, idSchema, fields) {
   // If the type is not defined and the schema uses 'anyOf' or 'oneOf', don't
   // render a field and let the MultiSchemaField component handle the form display
   if (!componentName && (schema.anyOf || schema.oneOf)) {
-    return () => null;
+    return NullComponent;
   }
 
   return componentName in fields
@@ -366,51 +368,55 @@ function SchemaFieldRender(props) {
         render the selection and let `StringField` component handle
         rendering
       */}
-        {schema.anyOf && !isSelect(schema) && (
-          <_AnyOfField
-            disabled={disabled}
-            readonly={readonly}
-            hideError={hideError}
-            errorSchema={errorSchema}
-            formData={formData}
-            idPrefix={idPrefix}
-            idSchema={idSchema}
-            idSeparator={idSeparator}
-            onBlur={props.onBlur}
-            onChange={props.onChange}
-            onFocus={props.onFocus}
-            options={schema.anyOf.map(_schema =>
-              retrieveSchema(_schema, rootSchema, formData)
-            )}
-            baseType={schema.type}
-            registry={registry}
-            schema={schema}
-            uiSchema={uiSchema}
-          />
-        )}
+        {FieldComponent === NullComponent &&
+          schema.anyOf &&
+          !isSelect(schema) && (
+            <_AnyOfField
+              disabled={disabled}
+              readonly={readonly}
+              hideError={hideError}
+              errorSchema={errorSchema}
+              formData={formData}
+              idPrefix={idPrefix}
+              idSchema={idSchema}
+              idSeparator={idSeparator}
+              onBlur={props.onBlur}
+              onChange={props.onChange}
+              onFocus={props.onFocus}
+              options={schema.anyOf.map(_schema =>
+                retrieveSchema(_schema, rootSchema, formData)
+              )}
+              baseType={schema.type}
+              registry={registry}
+              schema={schema}
+              uiSchema={uiSchema}
+            />
+          )}
 
-        {schema.oneOf && !isSelect(schema) && (
-          <_OneOfField
-            disabled={disabled}
-            readonly={readonly}
-            hideError={hideError}
-            errorSchema={errorSchema}
-            formData={formData}
-            idPrefix={idPrefix}
-            idSchema={idSchema}
-            idSeparator={idSeparator}
-            onBlur={props.onBlur}
-            onChange={props.onChange}
-            onFocus={props.onFocus}
-            options={schema.oneOf.map(_schema =>
-              retrieveSchema(_schema, rootSchema, formData)
-            )}
-            baseType={schema.type}
-            registry={registry}
-            schema={schema}
-            uiSchema={uiSchema}
-          />
-        )}
+        {FieldComponent === NullComponent &&
+          schema.oneOf &&
+          !isSelect(schema) && (
+            <_OneOfField
+              disabled={disabled}
+              readonly={readonly}
+              hideError={hideError}
+              errorSchema={errorSchema}
+              formData={formData}
+              idPrefix={idPrefix}
+              idSchema={idSchema}
+              idSeparator={idSeparator}
+              onBlur={props.onBlur}
+              onChange={props.onChange}
+              onFocus={props.onFocus}
+              options={schema.oneOf.map(_schema =>
+                retrieveSchema(_schema, rootSchema, formData)
+              )}
+              baseType={schema.type}
+              registry={registry}
+              schema={schema}
+              uiSchema={uiSchema}
+            />
+          )}
       </React.Fragment>
     </FieldTemplate>
   );
